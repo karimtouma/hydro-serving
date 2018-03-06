1cb652becbf101ab9c7d2d35790bb462d8e715f2
dmitryisaev <dmitryisaev@cloud.upwork.com>
2018-03-06 15:27:21 +0300

    start

---
+++ manager/src/main/scala/io/hydrosphere/serving/manager/service/envoy/xds/SubscriptionManagerActor.scala
@@ -0,0 +1,7 @@
+package io.hydrosphere.serving.manager.service.envoy.xds
+
+import akka.actor.{Actor, ActorLogging}
+
+class SubscriptionManagerActor extends Actor with ActorLogging{
+  override def receive: Receive = ???
+}
